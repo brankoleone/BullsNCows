195a593e9ebf23f5c73268c531ab82760106fff1
brankoleone <brankoleone@gmail.com>
2020-10-19 05:55:52 +0300

    Added a bull & google font

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2 className="title">Bulls'N'Cows</h2>
+        <img
+          src="https://www.pinclipart.com/picdir/big/525-5257803_charging-bull-clip-art-charging-bull-vector-png.png"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
